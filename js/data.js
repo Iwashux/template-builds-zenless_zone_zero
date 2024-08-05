68ffa5ea27296de65ba7c51514f23b24dac6f303
Iwashux <edu.silva.vil@gmail.com>
2024-08-05 01:18:58 -0400

    cambio disposicion de los datos, arreglos para imagenes predeterminado

--- js/data.js
+++ js/data.js
@@ -1,36 +1,77 @@
 var wEngeines = [
-    "Deep_Sea_Visitor", "Fusion_Compiler", "Hellfire_Gears",
-    "Riot_Suppressor_Mark_VI", "Steel_Cushion", "The_Brimstone",
-    "The_Restrained", "Weeping_Cradle",
+    {name: "Deep_Sea_Visitor", rarity: "s"},
+    {name: "Fusion_Compiler", rarity: "s"},
+    {name: "Hellfire_Gears", rarity: "s"},
+    {name: "Riot_Suppressor_Mark_VI", rarity: "s"},
+    {name: "Steel_Cushion", rarity: "s"},
+    {name: "The_Brimstone", rarity: "s"},
+    {name: "The_Restrained", rarity: "s"},
+    {name: "Weeping_Cradle", rarity: "s"},
+    
+    {name: "Bashful_Demon", rarity: "a"},
+    {name: "Big_Cylinder", rarity: "a"},
+    {name: "Bunny_Band", rarity: "a"},
+    {name: "Cannon_Rotor", rarity: "a"},
+    {name: "Demara_Battery_Mark_II", rarity: "a"},
+    {name: "Drill_Rig_-_Red_Axis", rarity: "a"},
+    {name: "Electro-Lip_Gloss", rarity: "a"},
+    {name: "Housekeeper", rarity: "a"},
+    {name: "Kaboom_the_Cannon", rarity: "a"},
+    {name: "Original_Transmorpher", rarity: "a"},
+    {name: "Precious_Fossilized_Core", rarity: "a"},
+    {name: "Rainforest_Gourmet", rarity: "a"},
+    {name: "Roaring_Ride", rarity: "a"},
+    {name: "Six_Shooter", rarity: "a"},
+    {name: "Slice_of_Time", rarity: "a"},
+    {name: "Spring_Embrace", rarity: "a"},
+    {name: "Starlight_Engine", rarity: "a"},
+    {name: "Starlight_Engine_Replica", rarity: "a"},
+    {name: "Steam_Oven", rarity: "a"},
+    {name: "Street_Superstar", rarity: "a"},
+    {name: "The_Vault", rarity: "a"},
+    {name: "Unfettered_Game_Ball", rarity: "a"},
+    {name: "Weeping_Gemini", rarity: "a"},
 
-    "Bashful_Demon", "Big_Cylinder", "Bunny_Band", "Cannon_Rotor",
-    "Demara_Battery_Mark_II", "Drill_Rig_-_Red_Axis", "Electro-Lip_Gloss",
-    "Housekeeper", "Kaboom_the_Cannon", "Original_Transmorpher",
-    "Precious_Fossilized_Core", "Rainforest_Gourmet", "Roaring_Ride",
-    "Six_Shooter", "Slice_of_Time", "Spring_Embrace", "Starlight_Engine",
-    "Starlight_Engine_Replica", "Steam_Oven", "Street_Superstar", "The_Vault",
-    "Unfettered_Game_Ball", "Weeping_Gemini",
-
-    "[Identity]_Base", "[Identity]_Inflection", "[Lunar]_Decrescent",
-    "[Lunar]_Noviluna", "[Lunar]_Pleniluna", "[Magnetic_Storm]_Alpha",
-    "[Magnetic_Storm]_Bravo", "[Magnetic_Storm]_Charlie", "[Reverb]_Mark_I",
-    "[Reverb]_Mark_II", "[Reverb]_Mark_III", "[Vortex]_Arrow", "[Vortex]_Hatchet",
-    "[Vortex]_Revolver"
+    {name: "[Identity]_Base", rarity: "b"},
+    {name: "[Identity]_Inflection", rarity: "b"},
+    {name: "[Lunar]_Decrescent", rarity: "b"},
+    {name: "[Lunar]_Noviluna", rarity: "b"},
+    {name: "[Lunar]_Pleniluna", rarity: "b"},
+    {name: "[Magnetic_Storm]_Alpha", rarity: "b"},
+    {name: "[Magnetic_Storm]_Bravo", rarity: "b"},
+    {name: "[Magnetic_Storm]_Charlie", rarity: "b"},
+    {name: "[Reverb]_Mark_I", rarity: "b"},
+    {name: "[Reverb]_Mark_II", rarity: "b"},
+    {name: "[Reverb]_Mark_III", rarity: "b"},
+    {name: "[Vortex]_Arrow", rarity: "b"},
+    {name: "[Vortex]_Hatchet", rarity: "b"},
+    {name: "[Vortex]_Revolver", rarity: "b"}
 ];
 
-var wEngeinesRarity = [ 
-    "s", "s", "s", "s", "s", "s", "s", "s",
-    "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", 
-    "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a",
-    "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b"
-];
 
+var bangboos = [
+    {name: "amillion", rarity: "s"},
+    {name: "bangvolver", rarity: "s"},
+    {name: "butler", rarity: "s"},
+    {name: "plugboo", rarity: "s"},
+    {name: "rocketboo", rarity: "s"},
+    {name: "safety", rarity: "s"},
+    {name: "sharkboo", rarity: "s"},
 
-var banfboos = [
-    "amillion", "avocaboo", "bagboo", "bangvolver", "boollseye", "booressure", 
-    "butler", "cryboo", "devilboo", "electroboo", "exploreboo", "luckyboo",
-    "magnetiboo", "paperboo", "penguinboo", "plugboo", "resonaboo",
-    "rocketboo", "safety", "sharkboo", "sumoboo"
+    {name: "avocaboo", rarity: "a"},
+    {name: "bagboo", rarity: "a"},
+    {name: "boollseye", rarity: "a"},
+    {name: "booressure", rarity: "a"},
+    {name: "cryboo", rarity: "a"},
+    {name: "devilboo", rarity: "a"},
+    {name: "electroboo", rarity: "a"},
+    {name: "exploreboo", rarity: "a"},
+    {name: "luckyboo", rarity: "a"},
+    {name: "magnetiboo", rarity: "a"},
+    {name: "paperboo", rarity: "a"},
+    {name: "penguinboo", rarity: "a"},
+    {name: "resonaboo", rarity: "a"},
+    {name: "sumoboo", rarity: "a"}
 ];
 
 var driveDisks = ["chaotic_metal", "fanged_metal", "freedom_blues", "hormone_punk",
@@ -39,140 +80,161 @@ var driveDisks = ["chaotic_metal", "fanged_metal", "freedom_blues", "hormone_pun
 ];
 
 var characters = [
-    "nekomata", "lycaon", "soldier_11", "koleda", "grace", "ellen_joe", 
-    "rina", "zhu_yuan", "qinqyi", "jane_doe",
+    {
+        name: "default",
+        rarity: "default",
+        attribute: "default",
+        specialty: "default",
+        faction: "default",
+        active: false
+    },{   
+        name: "nekomata",
+        rarity: "s",
+        attribute: "physical",
+        specialty: "attack",
+        faction: "cunning_hares",
+        active: true
+    },{
+        name: "lycaon",
+        rarity: "s",
+        attribute: "ice",
+        specialty: "stun",
+        faction: "victoria",
+        active: true
+    },{
+        name: "soldier_11",
+        rarity: "s",
+        attribute: "fire",
+        specialty: "attack",
+        faction: "obol_squad",
+        active: true
+    },{
+        name: "koleda",
+        rarity: "s",
+        attribute: "fire",
+        specialty: "stun",
+        faction: "belobog",
+        active: true
+    },{
+        name: "grace",
+        rarity: "s",
+        attribute: "electric",
+        specialty: "Anomaly",
+        faction: "belobog",
+        active: true
+    },{
+        name: "ellen_joe",
+        rarity: "s",
+        attribute: "ice",
+        specialty: "attack",
+        faction: "victoria",
+        active: true
+    },{
+        name: "rina",
+        rarity: "s",
+        attribute: "electric",
+        specialty: "support",
+        faction: "victoria",
+        active: true
+    },{
+        name: "zhu_yuan",
+        rarity: "s",
+        attribute: "ether",
+        specialty: "attack",
+        faction: "investigation_srt",
+        active: true
+    },{
+        name: "jane_doe",
+        rarity: "s",
+        attribute: "??",
+        specialty: "??",
+        faction: "??",
+        active: false
+    },{
+        name: "qinqyi",
+        rarity: "s",
+        attribute: "electric",
+        specialty: "stun",
+        faction: "investigation_srt",
+        active: false
+    },{
+        name: "hoshimi_miyabi",
+        rarity: "s",
+        attribute: "ice",
+        specialty: "attack",
+        faction: "section_6",
+        active: false
+    },
     
-    "anby", "billy", "nicole", "anton", "corin", 
-    "ben", "soukaku", "lucy", "piper", "hoshimi_miyabi",
-    "seth"
-];
-
-var charactersInfo = {  
-    "nekomata": {
-        "rarity": "s",
-        "attribute": "physical",
-        "specialty": "attack",
-        "faction": "cunning_hares",
-        "active": true
-    },"lycaon": {
-        "rarity": "s",
-        "attribute": "ice",
-        "specialty": "stun",
-        "faction": "victoria",
-        "active": true
-    },"soldier_11": {
-        "rarity": "s",
-        "attribute": "fire",
-        "specialty": "attack",
-        "faction": "obol_squad",
-        "active": true
-    },"koleda": {
-        "rarity": "s",
-        "attribute": "fire",
-        "specialty": "stun",
-        "faction": "belobog",
-        "active": true
-    },"grace": {
-        "rarity": "s",
-        "attribute": "electric",
-        "specialty": "Anomaly",
-        "faction": "belobog",
-        "active": true
-    },"ellen_joe": {
-        "rarity": "s",
-        "attribute": "ice",
-        "specialty": "attack",
-        "faction": "victoria",
-        "active": true
-    },"rina": {
-        "rarity": "s",
-        "attribute": "electric",
-        "specialty": "support",
-        "faction": "victoria",
-        "active": true
-    },"zhu_yuan": {
-        "rarity": "s",
-        "attribute": "ether",
-        "specialty": "attack",
-        "faction": "investigation_srt",
-        "active": true
-    },"anby": {
-        "rarity": "a",
-        "attribute": "electric",
-        "specialty": "stun",
-        "faction": "cunning_hares",
-        "active": true
-    },"billy": {
-        "rarity": "a",
-        "attribute": "physical",
-        "specialty": "attack",
-        "faction": "cunning_hares",
-        "active": true
-    },"nicole": {
-        "rarity": "a",
-        "attribute": "ether",
-        "specialty": "support",
-        "faction": "cunning_hares",
-        "active": true
-    },"anton": {
-        "rarity": "a",
-        "attribute": "electric",
-        "specialty": "attack",
-        "faction": "belobog",
-        "active": true
-    },"corin": {
-        "rarity": "a",
-        "attribute": "physical",
-        "specialty": "attack",
-        "faction": "victoria",
-        "active": true
-    },"ben": {
-        "rarity": "a",
-        "attribute": "fire",
-        "specialty": "defense",
-        "faction": "cunning_hares",
-        "active": true
-    },"soukaku": {
-        "rarity": "a",
-        "attribute": "ice",
-        "specialty": "support",
-        "faction": "section_6",
-        "active": true
-    },"lucy": {
-        "rarity": "a",
-        "attribute": "fire",
-        "specialty": "support",
-        "faction": "Calydon",
-        "active": true
-    },"piper": {
-        "rarity": "a",
-        "attribute": "physical",
-        "specialty": "Anomaly",
-        "faction": "Calydon",
-        "active": true
-    },"hoshimi_miyabi": {
-        "rarity": "s",
-        "attribute": "ice",
-        "specialty": "attack",
-        "faction": "section_6",
-        "active": false
-    },"qinqyi": {
-        "rarity": "s",
-        "attribute": "electric",
-        "specialty": "stun",
-        "faction": "investigation_srt",
-        "active": false
-    },"seth": {
-        "rarity": "a",
-        "attribute": "??",
-        "specialty": "??",
-        "faction": "??",
-        "active": false
-    },"jane_doe": {
-        "rarity": "s",
-        "attribute": "??",
-        "specialty": "??",
-        "faction": "??",
-        "active": false
+    {
+        name: "anby",
+        rarity: "a",
+        attribute: "electric",
+        specialty: "stun",
+        faction: "cunning_hares",
+        active: true
+    },{
+        name: "billy",
+        rarity: "a",
+        attribute: "physical",
+        specialty: "attack",
+        faction: "cunning_hares",
+        active: true
+    },{
+        name: "nicole",
+        rarity: "a",
+        attribute: "ether",
+        specialty: "support",
+        faction: "cunning_hares",
+        active: true
+    },{
+        name: "anton",
+        rarity: "a",
+        attribute: "electric",
+        specialty: "attack",
+        faction: "belobog",
+        active: true
+    },{
+        name: "corin",
+        rarity: "a",
+        attribute: "physical",
+        specialty: "attack",
+        faction: "victoria",
+        active: true
+    },{
+        name: "ben",
+        rarity: "a",
+        attribute: "fire",
+        specialty: "defense",
+        faction: "cunning_hares",
+        active: true
+    },{
+        name: "soukaku",
+        rarity: "a",
+        attribute: "ice",
+        specialty: "support",
+        faction: "section_6",
+        active: true
+    },{
+        name: "lucy",
+        rarity: "a",
+        attribute: "fire",
+        specialty: "support",
+        faction: "Calydon",
+        active: true
+    },{
+        name: "piper",
+        rarity: "a",
+        attribute: "physical",
+        specialty: "Anomaly",
+        faction: "Calydon",
+        active: true
+    },{
+        name: "seth",
+        rarity: "a",
+        attribute: "??",
+        specialty: "??",
+        faction: "??",
+        active: false
     }
-};
+];
--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
-let scale = 1;
+// let scale = 1;
 
-// redimencion lienso
-function fixWidth() { 
-    let canvas = $("main");
-    let canvasHeight = canvas.height();
-    let canvasWidth = canvas.width();
+// // redimencion lienso
+// function fixWidth() { 
+//     let canvas = $("main");
+//     let canvasHeight = canvas.outerHeight(true);
+//     let canvasWidth = canvas.width();
 
-    while ($(window).height() - 50 <= canvasHeight * scale || 
-        $(window).width() - 50 <= canvasWidth * scale) {
-        scale -= 0.01;
-    }
+//     while ($(window).height() <= canvasHeight * scale || 
+//         $(window).width() <= canvasWidth * scale) {
+//         scale -= 0.01;
+//     }
 
-    while ($(window).height() - 50 >= canvasHeight * scale &&
-        $(window).width() - 50 >= canvasWidth * scale) {
-        scale += 0.01;
-    }
+//     while ($(window).height() >= canvasHeight * scale &&
+//         $(window).width() >= canvasWidth * scale) {
+//         scale += 0.01;
+//     }
 
-    canvas.css("transform", "scale(" + scale + ")");
-    console.log(scale);
+//     canvas.css("transform", "scale(" + scale + ")");
+//     console.log(scale);
     
-}fixWidth();
+// }fixWidth();
 
 $(".cinema__switch__button").on("click", function() {
     let element = $(this);
@@ -62,6 +62,25 @@ function checkElements() {
     });
 }checkElements();
 
+var activeTimeout;
+
+$("#w-engine, #drive-disk, #team").on("click", function() {
+    var element = $(this);
+    $("#w-engine, #drive-disk, #team").removeClass("conteiner__active");
+
+    // Detener la animación anterior si está en curso
+    if (activeTimeout) {
+        clearTimeout(activeTimeout);
+    }
+
+    element.addClass("conteiner__active");
+
+    // Iniciar nueva animación
+    activeTimeout = setTimeout(function() {
+        element.removeClass("conteiner__active");
+    }, 1500);
+});
+
 function putWEngine() {
     console.log(wEngeines);
 
@@ -69,9 +88,9 @@ function putWEngine() {
 
     wEngeines.forEach(function(wEngeine, index) {
         selectedComponent.append(
-            `<div class="w-engine__conteiner">
-                <img class="w-engine__rarity" src="img/ranks/item_rank_${wEngeinesRarity[index]}.webp" alt="">
-                <img class="w-engine__img" src="img/w-engine/${wEngeine}.webp" alt="">
+            `<div class="w-engine__conteiner" id_w-engine-data='${index}'>
+                <img class="w-engine__rarity" src="img/ranks/item_rank_${wEngeine.rarity}.webp" alt="">
+                <img class="w-engine__img" src="img/w-engine/${wEngeine.name}.webp" alt="">
             </div>`
         );
     });
@@ -85,16 +104,16 @@ function putCharacter() {
     const selectedCharacter = $(".selected__character__conteiner");
 
     characters.forEach(function(character, index) {
-        let characterText = capitalizeEachWord(character.replaceAll("_"," "));
+        let characterText = capitalizeEachWord(character.name.replaceAll("_"," "));
 
-        if (charactersInfo[character].active) {
+        if (character.active) {
             selectedCharacter.append(
                 `<div class='character__avatar' id_character-data='${index}'>
                     <div class="team__character__info">
-                        <img src="img/ranks/char_rank_${charactersInfo[character].rarity}_color.png" alt="">
-                        <img src="img/attributes/${charactersInfo[character].attribute}.png" alt="">
+                        <img src="img/ranks/char_rank_${character.rarity}_color.png" alt="">
+                        <img src="img/attributes/${character.attribute}.png" alt="">
                     </div>
-                    <img src="img/char_avatar/${character}.png" alt="">
+                    <img src="img/char_avatar/${character.name}.png" alt="">
                     <p class="team__characters__name">${characterText}</p>
                 </div>`
             );
@@ -116,11 +135,14 @@ function capitalize(string) {
 $(document).on("click", ".character__avatar", function() {
     idCharacter = $(this).attr("id_character-data");
     character = characters[idCharacter];
-    let characterText = capitalizeEachWord(character.replaceAll("_"," "));
+
+    let characterText = capitalizeEachWord(character.name.replaceAll("_"," "));
     
     $("#character-name").text(characterText);
-    $("#character-faction").attr("src", `img/factions/${charactersInfo[character].faction}.png`);
-    $("#character-attribute").attr("src", `img/attributes/${charactersInfo[character].attribute}.png`);
-    $("#character-specialty").attr("src", `img/specialties/${charactersInfo[character].specialty}.png`);
-    $("#character-rank").attr("src", `img/ranks/char_rank_${charactersInfo[character].rarity}.png`);
+    $("#character-info-attributes").removeClass("default");
+    $("#character-faction").attr("src", `img/factions/${character.faction}.png`);
+    $("#character-attribute").attr("src", `img/attributes/${character.attribute}.png`).removeClass();
+    $("#character-specialty").attr("src", `img/specialties/${character.specialty}.png`).removeClass();
+
+    $("#character-rank").attr("src", `img/ranks/char_rank_${character.rarity}.png`);
 });
